b913a0816f21e0a0790471933c436ae1942d12c7
Taner Selim <taner.selim@gameforge.com>
2023-12-11 19:37:59 +0100

    new stuff

--- pkg/game/game_server.go
+++ pkg/game/game_server.go
@@ -1,4 +1,27 @@
 package game
 
+import (
+	"time"
+
+	"github.com/tanerius/EventGoRound/eventgoround"
+	"github.com/tanerius/dungeonforge/pkg/database"
+	"github.com/tanerius/dungeonforge/pkg/server"
+)
+
 type GameServer struct {
+	playerChannels map[string]chan *GameMessageEvent
+	hub            *server.Coordinator
+	db             *database.DBWrapper
+	em             *eventgoround.EventManager
+}
+
+func NewGameServer(_h *server.Coordinator) *GameServer {
+	return &GameServer{
+		playerChannels: make(map[string]chan *GameMessageEvent),
+		hub:            _h,
+	}
+}
+
+func (g *GameServer) Run() {
+	ticker := time.NewTicker(10 * time.Second)
 }
