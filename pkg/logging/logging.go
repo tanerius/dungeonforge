a6856e9b9805163809c8fb5450407e3c05e2317b
Taner Selim <taner,selim@gameforge.com>
2024-05-13 12:56:16 +0200

    Prevent mongo express from annoyingly autostarting

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -1,6 +1,8 @@
 package logging
 
 import (
+	"sync"
+
 	"github.com/rs/zerolog/pkgerrors"
 
 	"github.com/rs/zerolog"
@@ -24,26 +26,35 @@ type ILogger interface {
 }
 
 type Logger struct {
+	mu       sync.Mutex
 	hadError bool
 }
 
 // The main logging function for Error
 func (h *Logger) LogError(err error, extraMsg string) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
 	log.Error().Stack().Err(err).Msg(extraMsg)
 }
 
 // The main logging function for Info
 func (h *Logger) LogInfo(msg string) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
 	log.Info().Msg(msg)
 }
 
 // The main logging function for Trace
 func (h *Logger) LogTrace(msg string) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
 	log.Trace().Msg(msg)
 }
 
 // Error testing
 func (h *Logger) HadError() bool {
+	h.mu.Lock()
+	defer h.mu.Unlock()
 	return h.hadError
 }
 
